dafcca9f299c3172f9b413e13b16eb07213bacd0
Yasuyuki Arakawa <b.a520y@gmail.com>
2020-07-21 08:53:36 +0900

    frame size 100%

--- frontend/src/views/Camera.vue
+++ frontend/src/views/Camera.vue
@@ -2,9 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Vue Video Streaming Demonstration</h1>
-    <img src="/video_feed">
+    <img src="/video_feed" class="frame">
   </div>
 </template>
+
+<style scoped>
+.frame {
+  width: 100%;
+  height: 100%;
+}
+</style>
 
 <script>
   export default {
